16a02fc30ef8583dd74399a9123079a881275a27
Alecs Popa <alecs.popa@gmail.com>
2016-08-05 15:51:52 +0300

    Add Elm

--- brunch-config.js
+++ brunch-config.js
@@ -42,7 +42,9 @@ exports.config = {
     // Dependencies and current project directories to watch
     watched: [
       "web/static",
-      "test/static"
+      "test/static",
+      "web/elm/src",
+      "web/elm/test"
     ],
 
     // Where to compile files to
@@ -54,7 +56,12 @@ exports.config = {
     babel: {
       // Do not use ES6 compiler in vendor code
       ignore: [/web\/static\/vendor/]
-    }
+    },
+    elmBrunch: {
+      elmFolder: 'web/elm',
+      mainModules: ['Main.elm'],
+      outputFolder: '../static/vendor'
+    },
   },
 
   modules: {
