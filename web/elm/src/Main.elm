0aab1ee7f1a663bffeae0ea7f1f15dcf0eeb309e
Alecs Popa <alecs.popa@gmail.com>
2016-08-09 00:32:02 +0300

    Elm demo

--- web/elm/src/Main.elm
+++ web/elm/src/Main.elm
@@ -1,6 +1,76 @@
 module Main exposing (..)
 
-import Html
+import Html exposing (..)
+import Html.App as Html
+import Html.Attributes exposing (class)
+import Html.Events exposing (onClick)
 
+main : Program Never
 main =
-  Html.text "Hello from Elm"
+    Html.program
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = \_ -> Sub.none
+        }
+
+-- MODEL
+
+type alias Seat =
+    { seatNo: Int
+    , occupied: Bool
+    }
+
+type alias Model =
+    List Seat
+
+init : (Model, Cmd Msg)
+init =
+    let seats =
+        [ { seatNo = 1, occupied = False }
+        , { seatNo = 2, occupied = False }
+        , { seatNo = 3, occupied = False }
+        , { seatNo = 4, occupied = False }
+        , { seatNo = 5, occupied = False }
+        , { seatNo = 6, occupied = False }
+        , { seatNo = 7, occupied = False }
+        , { seatNo = 8, occupied = False }
+        , { seatNo = 9, occupied = False }
+        , { seatNo = 10, occupied = False }
+        , { seatNo = 11, occupied = False }
+        , { seatNo = 12, occupied = False }
+        ]
+    in (seats, Cmd.none)
+
+-- UPDATE
+
+type Msg = Toogle Seat
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        Toogle seatToToggle ->
+            let updateSeat seatFromModel =
+                if seatFromModel.seatNo == seatToToggle.seatNo then
+                    { seatFromModel | occupied = not seatFromModel.occupied }
+                else seatFromModel
+            in
+                (List.map updateSeat model, Cmd.none)
+
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+    ul [ class "seats" ] (List.map seatItem model)
+
+seatItem : Seat -> Html Msg
+seatItem seat =
+    let occupiedClass =
+        if seat.occupied then "occupied" else "available"
+    in
+        li
+        [ class ("seat " ++ occupiedClass)
+        , onClick (Toogle seat)
+        ]
+        [ text (toString seat.seatNo) ]
